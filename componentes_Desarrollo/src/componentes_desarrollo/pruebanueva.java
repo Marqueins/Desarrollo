8f02148621ab854665d0d5f7029431ccd1a961fe
MARCOS <MARCOS@MARKEINS>
2016-11-09 22:34:03 -0500

    marcosAlP1

---
+++ componentes_Desarrollo/src/componentes_desarrollo/pruebanueva.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package componentes_desarrollo;
+
+/**
+ *
+ * @author MARCOS
+ */
+public class pruebanueva {
+    // prueba prueba
+    
+    
+}
